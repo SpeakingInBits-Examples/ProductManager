1f02b55ffd97ace04660f4f829e345afa4577a0e
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2025-11-18 11:40:19 -0800

    Add Product class with properties and documentation
    Added a `Product`
    class to represent products for sale. The class includes `Id`,
    `SalesPrice`, and `Name` properties

---
+++ ProductManager/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace ProductManager;
+
+/// <summary>
+/// Represents a single product that
+/// is for sale.
+/// </summary>
+public class Product
+{
+    /// <summary>
+    /// The unique primary key identifier for the product
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The price the customer will be charged for this
+    /// product. This is US currency and should be rounded
+    /// to the nearest penny.
+    /// </summary>
+    public double SalesPrice { get; set; }
+
+    /// <summary>
+    /// The customer facing name of the product.
+    /// </summary>
+    public required string Name { get; set; }
+}
