5e7f0cbef01e732d9a74a2387c909a61aeb861f7
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2025-11-25 11:07:19 -0800

    Add ProductDb class with method stubs for product management operations
    Implemented method stubs for `GetAllProducts`, `AddProduct`, `UpdateProduct`, and two overloads of `DeleteProduct`. All methods currently throw `NotImplementedException`.

---
+++ ProductManager/ProductDb.cs
@@ -0,0 +1,39 @@
+﻿using ProductManager.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager;
+
+public static class ProductDb
+{
+    /// <summary>
+    /// This will return all products from the database
+    /// sorted in ascending order by Name.
+    /// </summary>
+    /// <returns></returns>
+    public static List<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void AddProduct(Product p)
+    {
+        throw new NotImplementedException(); 
+    }
+
+    public static void UpdateProduct(Product p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DeleteProduct(Product p) 
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DeleteProduct(int productId)
+    {
+        throw new NotImplementedException();
+    }
+}
